f8e5456daf74d2bb4f45f73bd0c6cf14f7a9149f
Gabriel Galdino <gabrielgaldino7@yahoo.com>
2019-11-04 00:45:32 -0300

    Cria página index.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TodoList</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+    <footer id="footer" class="text-center">
+      <p>Copyright &Copy; 2019 TodoList</p>
+    </footer>
+  </body>
+</html>
